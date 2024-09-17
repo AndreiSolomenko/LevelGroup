edb460c9210f3f81ca8c3aa7ae1e8a95554daf20
AndreiSolomenko <andrsolo10@gmail.com>
2024-09-17 11:17:50 +0300

    add sql del

---
+++ src/main/resources/db.migration/V11__delete_data.sql
@@ -0,0 +1,5 @@
+-- Видалення з таблиці LGObject за internalId
+DELETE FROM LGObject WHERE internalId = '212964781';
+
+-- Видалення з таблиці RiaLGObject за localRealtyId
+DELETE FROM RiaLGObject WHERE localRealtyId = '212964781';
