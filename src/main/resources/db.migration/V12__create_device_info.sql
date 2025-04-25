b776455f2ba72ff95cb337f1ff4eb20ba0a0f412
AndreiSolomenko <andrsolo10@gmail.com>
2025-04-25 11:05:07 +0300

    fix migration for registration

--- src/main/resources/db.migration/V12__create_device_info.sql
+++ src/main/resources/db.migration/V12__create_device_info.sql
@@ -4,7 +4,7 @@ CREATE TABLE device_info (
     email VARCHAR(255),
     check_counter INT DEFAULT 0,
     permanently_activated BOOLEAN DEFAULT FALSE,
-    temporarily_activated BOOLEAN DEFAULT TRUE,
+    temporarily_activated BOOLEAN,
     country VARCHAR(255),
     subscription_until DATE
 );
